1d89d57d3ac4835fd96155b7666e56f16083bb61
Szymon Truszkowski <struszkowski98@gmail.com>
2023-04-27 08:40:21 +0200

    updateBoard method added

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -5,6 +5,8 @@ public class TicTacToe {
         char[][] board = {{'_','|','_','|','_'}, {'_','|','_','|','_'}, {'_','|','_','|','_'}};
 
         printBoard(board);
+        updateBoard(board, 1, 1);
+        updateBoard(board, 2, 2);
     }
 
     public static void printBoard(char[][] board) {
@@ -14,6 +16,52 @@ public class TicTacToe {
                 System.out.print(c);
             }
             System.out.println();
+        }
+    }
+
+    public static void updateBoard(char[][] board, int player, int move) {
+
+        char symbol = player == 1 ? 'X' : 'O';
+
+        switch (move) {
+            case 1 -> {
+                board[0][0] = symbol;
+                printBoard(board);
+            }
+            case 2 -> {
+                board[0][2] = symbol;
+                printBoard(board);
+            }
+            case 3 -> {
+                board[0][4] = symbol;
+                printBoard(board);
+            }
+            case 4 -> {
+                board[1][0] = symbol;
+                printBoard(board);
+            }
+            case 5 -> {
+                board[1][2] = symbol;
+                printBoard(board);
+            }
+            case 6 -> {
+                board[1][4] = symbol;
+                printBoard(board);
+            }
+            case 7 -> {
+                board[2][0] = symbol;
+                printBoard(board);
+            }
+            case 8 -> {
+                board[2][2] = symbol;
+                printBoard(board);
+            }
+            case 9 -> {
+                board[2][4] = symbol;
+                printBoard(board);
+            }
+            default -> {
+            }
         }
     }
 
