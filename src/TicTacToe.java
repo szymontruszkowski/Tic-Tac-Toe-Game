95c8b5cf49aebc180069040845ee6c204a243c30
Szymon Truszkowski <struszkowski98@gmail.com>
2023-05-12 12:10:29 +0200

    documentation comments updated

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -62,7 +62,7 @@ public class TicTacToe {
     /**
      * Update the board with X or O
      * @param board     the board to be updated
-     * @param player    the player who is making a move
+     * @param player    the player who makes a move
      * @param move      the move (chosen slot of the board)
      */
     private static void updateBoard(char[][] board, int player, int move) {
